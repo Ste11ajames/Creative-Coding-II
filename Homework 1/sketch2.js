e888bd5cf04b62d17159bb546f15ac07c6e5508d
Stella Smith <112897475+Ste11ajames@users.noreply.github.com>
2025-01-26 16:06:48 -0700

    push all to orgin

---
+++ Homework 1/sketch2.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(400, 400);
+    noStroke(); 
+  
+   
+    fill(200, 150, 100); 
+    ellipse(width / 2, height / 2, 200); 
+  
+    
+    fill(255, 255, 255); 
+    ellipse(width / 2, height / 2, 100); 
+  }
