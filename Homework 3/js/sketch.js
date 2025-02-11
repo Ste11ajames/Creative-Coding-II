c21c8d85ae4ec87a8041d0662426f546d6619535
Stella Smith <112897475+Ste11ajames@users.noreply.github.com>
2025-02-10 17:35:10 -0700

    move

--- Homework 3/js/sketch.js
+++ Homework 3/js/sketch.js
@@ -1,31 +1,55 @@
+let fonts = ["Georgia", "Courier New", "Times New Roman", "Verdana", "Arial"];
+let chosenFont; 
 
+let circleX, circleY; 
+let lastMoveTime = 0; 
+let moveInterval = 2000; //how often circle moves
 
 function setup() {
   createCanvas(800, 800);
   noStroke();
 
-}
+  chosenFont = random(fonts); //random font on refresh
 
+  // starting point for random circle
+  circleX = random(width);
+  circleY = random(height);
+}
 
 function draw() {
   background(200, 60, 16);
 
-  
-
   // Draw the bagel
   drawBagel(width / 2, height / 2, 200, 80);
 
+  //  moving circle
+  updateCircle();
+  drawCircle();
 
-
-  // Draw text
-  fill(255);
+  // Draw text with the chosen random font
+  fill(25,444,55);
   textSize(20);
+  textFont(chosenFont);
   text("Homework 3", 10, 30); // Top left
 
   textSize(25);
-  text("Stella Smith", width - 140, height - 10); // Bottom right
+  text("Stella S.", width - 140, height - 10); // Bottom right
 }
 
+
+function updateCircle() {
+  if (millis() - lastMoveTime > moveInterval) {
+    circleX = random(width); // Moves circle to random position
+    circleY = random(height);
+    lastMoveTime = millis(); // Reset the timer
+  }
+}
+
+// moving circle
+function drawCircle() {
+  fill(55, 55, 66); 
+  ellipse(circleX, circleY, 62, 62);
+}
 
 function drawBagel(x, y, outerDiameter, innerDiameter) {
   fill(237, 192, 121);
@@ -37,11 +61,8 @@ function drawBagel(x, y, outerDiameter, innerDiameter) {
   drawSeeds(x, y, outerDiameter, innerDiameter);
 }
 
-
-
-
 function drawSeeds(x, y, outerDiameter, innerDiameter) {
-  fill(220, 180, 90);
+  fill(10, 10, 10);
   let seedCount = 50;
 
   for (let i = 0; i < seedCount; i++) {
@@ -56,6 +77,4 @@ function drawSeeds(x, y, outerDiameter, innerDiameter) {
     ellipse(0, 0, 4, 10);
     pop();
   }
-
-  
 }
