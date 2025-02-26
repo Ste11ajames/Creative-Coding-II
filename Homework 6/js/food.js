b9edb9fbf99907de21bd3b189264c8c34a29cc47
Stella Smith <112897475+Ste11ajames@users.noreply.github.com>
2025-02-26 15:29:06 -0700

    push

---
+++ Homework 6/js/food.js
@@ -0,0 +1,13 @@
+class food {
+    constructor(x,y)
+    {
+        this.x = x;
+        this.y = y;
+
+    }
+    draw() {
+        fill(33, 100, 67);
+       circle(this.x, this.y, 40);
+       
+    }
+}
---
+++ Homework 6/js/sketch.js
@@ -0,0 +1,108 @@
+var myCharacter;
+var myFood;
+var x=100;
+var y=100;
+var foodArray =[];
+var foodfound = false;
+var animation = [];
+var runAnimation = [];
+var idleStrings =[];
+
+var i = 0;
+function preload(){
+
+    idleStrings=loadStrings("data/idle.txt")
+console.log(idleStrings.length)
+   
+    
+   // setInterval(updateIndex, 50);
+    //for(let i =0; i < 5; i++){
+//place my food random
+   // }
+    
+}
+
+
+function setup()
+{
+    createCanvas(800, 800);
+    setInterval(updateIndex, 50);
+    for (let i = 0; i < 5; i++) {
+        myFood = new food(random(100, 600), random(100, 600), 25);
+        foodArray.push(myFood);
+    }
+    for(let i = 0; i< idleStrings.length; i++)
+        {
+            myCharacter = new character(idleStrings[i]); 
+            // i + is the concatenation - adding number to string - adding strings together
+            //place png images here
+            animation.push(myCharacter);
+            
+            myCharacter = new character("images/Run (" + i +").png"); 
+        
+            runAnimation.push(myCharacter); //add run strings
+
+        }
+}
+
+function draw() {
+
+    background(120);
+    console.log(animation.length);
+   
+ 
+
+    for (let i = 0; i < foodArray.length; i++) {
+        foodArray[i].draw();
+    }
+
+    if (keyIsPressed) {
+        runAnimation[i].draw();
+        if (key == "a") {
+            x--;
+        }
+        if (key == "d") {
+            x++;
+        }
+        if (key == "w") {
+            y--;
+        }
+        if (key == "s") {
+            y++;
+        }
+
+        
+        
+
+        for (let i = 0; i < animation.length; i++) {
+            animation[i].x = x;
+            animation[i].y = y;
+            runAnimation[i].x =x;
+            runAnimation[i].y =y;
+        }
+
+        for (let k = 0; k < foodArray.length; k++) {
+            if (animation[i].hasCollided(foodArray[k].x, foodArray[k].y, 25, 25)) {
+                foodArray.splice(k, 1);
+                
+            }
+            
+           
+            
+        }
+        
+        
+    }
+    else {
+        animation[i].draw();
+    }
+}
+
+function updateIndex() 
+{
+    i++;
+    if (i > animation.length - 1) {
+        i = 0;
+    }
+
+}
